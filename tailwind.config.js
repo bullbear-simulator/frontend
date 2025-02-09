3479f24317f3c5b13b582ab9e36544a4d1cf3457
khk99 <khk990815@gmail.com>
2025-02-09 15:29:25 +0900

    💄 앱 색상 및 폰트 정의

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,45 @@
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        stock: ["Poppins", "sans-serif"], // 종목명
+        body: ["Lato", "sans-serif"], // 일반 text
+        number: ["Roboto Mono", "monospace"], // 가격 등 숫자
+      },
+      color: {
+        primary_txt: {
+          // 기본 글자색
+          default: "#1E293B",
+          dark: "#E2E8F0",
+        },
+        primary_bg: {
+          // 기본 배경색
+          default: "#F8FAFC",
+          dark: "#0F172A",
+        },
+        secondary_txt: {
+          // 서브 글자색
+          default: "#64748B",
+          dark: "#94A3B8",
+        },
+        secondary_bg: {
+          // 서브 배경색
+          default: "#E1E6EE",
+          dark: "#1C2530",
+        },
+        bull: {
+          // 양봉, 매수 등 빨간색
+          default: "#FF4C4C",
+          dark: "#FF6B6B",
+        },
+        bear: {
+          // 음봉, 매도 등 파란색
+          default: "#007BFF",
+          dark: "#1E90FF",
+        },
+      },
+    },
   },
   plugins: [],
 };
