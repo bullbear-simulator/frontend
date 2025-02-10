2eb8d60022c223564ed149fed76aa594543609bc
khk99 <khk990815@gmail.com>
2025-02-10 19:14:35 +0900

    ✨ react-helmet 추가 및 Login-Callback 함수 생성

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import "./App.css";
 import "typeface-poppins";
 import "typeface-lato";
 import "typeface-roboto-mono";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Login, Callback } from "./pages/login";
 
 function App() {
   return (
     <>
-      <h1 className="text-red-500 text-[200px]">왜? 안되니</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />{" "}
+          {/* component prop을 element prop으로 변경 */}
+          <Route path="/auth/kakao" element={<Callback />} />{" "}
+          {/* component prop을 element prop으로 변경 */}
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,4 +1,5 @@
-import Helmet from "helmet";
+import Helmet from "react-helmet";
+import { useEffect } from "react";
 
 function Login() {
   const KAKAO_REST_API_KEY = "08f6fb6d41c553e165e98c0201582fdd";
@@ -6,15 +7,72 @@ function Login() {
 
   const loginHandler = () => {
     const url = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-    window.location.href = url; // 카카오 로그인 페이지로 이동
+    window.location.href = url;
   };
 
   return (
     <>
-      <Helmet></Helmet>
+      <Helmet>
+        <title>로그인</title>
+        <meta name="description" content="카카오 로그인 페이지" />
+      </Helmet>
       <button onClick={loginHandler}>카카오</button>
     </>
   );
 }
 
-export default Login;
+function Callback() {
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code");
+
+    if (code) {
+      fetch("https://kauth.kakao.com/oauth/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          grant_type: "authorization_code",
+          client_id: "08f6fb6d41c553e165e98c0201582fdd",
+          redirect_uri: "http://localhost:8080/auth/kakao",
+          code: code,
+        }),
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error("Token request failed");
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log("Access Token:", data.access_token);
+          fetchUserInfo(data.access_token);
+        })
+        .catch(error => console.error("Error:", error));
+    }
+  }, []);
+
+  const fetchUserInfo = accessToken => {
+    fetch("https://kapi.kakao.com/v2/user/me", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("User info request failed");
+        }
+        return response.json();
+      })
+      .then(userData => {
+        console.log("User Data:", userData);
+      })
+      .catch(error => console.error("Error:", error));
+  };
+
+  return <div>로그인 중...</div>;
+}
+
+export { Login, Callback };
