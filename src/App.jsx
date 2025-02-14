e44956c590cbaccb69f167c3f118c017b82990a6
khk99 <khk990815@gmail.com>
2025-02-14 17:01:05 +0900

    ♻️ 로그인 경로를 / 에서 /login으로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/auth/kakao" element={<Callback />} />
           <Route path="/home" element={<Home />} />
         </Routes>
