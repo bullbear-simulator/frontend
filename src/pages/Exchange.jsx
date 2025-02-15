43440638ed5e811d012696c8d15c3b6942c59b5d
sunghyeon <ggjj7827@gmail.com>
2025-02-15 20:39:11 +0900

    💄 거래소 화면 구성 완료

--- src/pages/Exchange.jsx
+++ src/pages/Exchange.jsx
@@ -1,11 +1,99 @@
 function Exchange() {
   return (
-    <div>
-      <h1>Exchange</h1>
-      <h1>Exchange</h1>
-      <h1>Exchange</h1>
-      <h1>Exchange</h1>
-    </div>
+    <main className="max-w-iphone-14-pro-max text-primary_txt-default">
+      <div className="flex gap-10 justify-center bg-secondary_bg-default">
+        <p>종목명</p>
+        <p>현재가</p>
+        <p>전일대비</p>
+        <p>거래량</p>
+      </div>
+      <div className="flex flex-col items-center gap-5 bg-primary_bg-default">
+        <div className="flex gap-10 justify-end items-center">
+          <p>종목이름</p>
+          <p>1,111</p>
+          <p>1,00%</p>
+          <p>1,234,567</p>
+        </div>
+        <div className="flex gap-10 justify-end items-center">
+          <p>종목이름</p>
+          <p>1,111</p>
+          <p>1,00%</p>
+          <p>1,234,567</p>
+        </div>
+        <div className="flex gap-10 justify-end items-center">
+          <p>종목이름</p>
+          <p>1,111</p>
+          <p>1,00%</p>
+          <p>1,234,567</p>
+        </div>
+        <div className="flex gap-10 justify-end items-center">
+          <p>종목이름</p>
+          <p>1,111</p>
+          <p>1,00%</p>
+          <p>1,234,567</p>
+        </div>
+        <div className="flex gap-10 justify-end items-center">
+          <p>종목이름</p>
+          <p>1,111</p>
+          <p>1,00%</p>
+          <p>1,234,567</p>
+        </div>
+        <div className="flex gap-10 justify-end items-center">
+          <p>종목이름</p>
+          <p>1,111</p>
+          <p>1,00%</p>
+          <p>1,234,567</p>
+        </div>
+        <div className="flex gap-10 justify-end items-center">
+          <p>종목이름</p>
+          <p>1,111</p>
+          <p>1,00%</p>
+          <p>1,234,567</p>
+        </div>
+        <div className="flex gap-10 justify-end items-center">
+          <p>종목이름</p>
+          <p>1,111</p>
+          <p>1,00%</p>
+          <p>1,234,567</p>
+        </div>
+        <div className="flex gap-10 justify-end items-center">
+          <p>종목이름</p>
+          <p>1,111</p>
+          <p>1,00%</p>
+          <p>1,234,567</p>
+        </div>
+        <div className="flex gap-10 justify-end items-center">
+          <p>종목이름</p>
+          <p>1,111</p>
+          <p>1,00%</p>
+          <p>1,234,567</p>
+        </div>
+        <div className="flex gap-10 justify-end items-center">
+          <p>종목이름</p>
+          <p>1,111</p>
+          <p>1,00%</p>
+          <p>1,234,567</p>
+        </div>
+        <div className="flex gap-10 justify-end items-center">
+          <p>종목이름</p>
+          <p>1,111</p>
+          <p>1,00%</p>
+          <p>1,234,567</p>
+        </div>
+        <div className="flex gap-10 justify-end items-center">
+          <p>종목이름</p>
+          <p>1,111</p>
+          <p>1,00%</p>
+          <p>1,234,567</p>
+        </div>
+        <div className="flex gap-10 justify-end items-center">
+          <p>종목이름</p>
+          <p>1,111</p>
+          <p>1,00%</p>
+          <p>1,234,567</p>
+        </div>
+      </div>
+    </main>
   );
 }
 
