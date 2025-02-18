0e9c010bec18b359c956916caa11f7e153b2ed44
sunghyeon <ggjj7827@gmail.com>
2025-02-18 16:43:02 +0900

    🚧 공용 헤더, 푸터 화면 구성작업 진행중

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,8 @@
+
+function Footer() {
+  return (
+    <main>푸터</main>
+  );
+}
+
+export default Footer; // 기본 내보내기
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,8 @@
+
+function Header() {
+  return (
+    <main>헤더</main>
+  );
+}
+
+export default Header; // 기본 내보내기
