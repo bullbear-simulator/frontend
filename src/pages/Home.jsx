e71727faf88556fd98e26577e34f494cc94db0b0
sunghyeon <ggjj7827@gmail.com>
2025-02-21 17:38:22 +0900

    ✨ useAxiosInstance.js 파일 추가 완료

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,4 @@
-import { useEffect } from 'react';
-import useAxiosInstance from '../hooks/useAxiosInstance';
-
 function Home() {
-  useEffect(() => {
-    // 데이터 가져오기
-    const fetchData = async () => {
-      try {
-        const response = await useAxiosInstance.get('/stocks');
-        console.log('데이터:', response.data);
-      } catch (error) {
-        console.error('데이터 가져오기 오류:', error);
-      }
-    };
-    console.log("123");
-
-    fetchData();
-  }, []); // 빈 배열을 주면 컴포넌트가 처음 마운트될 때만 실행됨
 
   return (
     <h1>Home</h1>
